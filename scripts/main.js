09001dfb86fdecf0503f32b63a2d3205658aeaa0
Zacj Mor <zackmortar9@gmail.com>
2024-11-05 14:21:42 +0200

    added animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 const arrowdownhero = document.querySelector(".hero svg");
 let previousY = window.scrollY;
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(CustomEase);
 gsap.to(".hero svg", {
 	y: -3,
 	duration: 0.35,
@@ -29,6 +30,48 @@ gsap.to(".hero svg", {
 		}
 	);
 });
+
+gsap.fromTo(
+	".levelup > h2",
+	{
+		fontSize: "7rem",
+	},
+	{
+		fontSize: "3rem",
+		duration: 4,
+		ease: CustomEase.create(
+			"custom",
+			"M0,0 C0,0 0.012,-0.018 0.365,0 0.505,0.007 0.546,0.433 0.713,0.685 0.843,0.883 1,1 1,1 "
+		),
+		scrollTrigger: {
+			trigger: ".levelup > h2",
+			start: "top 100%",
+			toggleActions: "play none none reverse",
+		},
+	}
+);
+
+gsap.fromTo(
+	".levelup button",
+	{
+		opacity: 0,
+		y: -50,
+	},
+	{
+		y: 30,
+		opacity: 1,
+		duration: 6,
+		ease: CustomEase.create(
+			"custom",
+			"M0,0 C0,0 0.012,-0.018 0.365,0 0.505,0.007 0.546,0.433 0.713,0.685 0.843,0.883 1,1 1,1 "
+		),
+		scrollTrigger: {
+			trigger: ".levelup > h2",
+			start: "top 100%",
+			toggleActions: "play none none reverse",
+		},
+	}
+);
 
 //arrow fade
 arrowdownhero.style.opacity = 1;
