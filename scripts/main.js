093bc0405095d494ef23b540b1e599dae48044f1
Zacj Mor <zackmortar9@gmail.com>
2024-11-05 15:08:06 +0200

    fixed responsivity bugs

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 const arrowdownhero = document.querySelector(".hero svg");
 let previousY = window.scrollY;
+// document.getElementById("ourworkbtn").addEventListener("click", () => {
+// 	document.getElementById('whyus').scrollIntoView({ behavior: 'smooth' });
+// });
+// document.getElementById("aboutusbtn").addEventListener("click", () => {
+// 	document.getElementById('expertise').scrollIntoView({ behavior: 'smooth' });
+// });
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(CustomEase);
 gsap.to(".hero svg", {
@@ -31,47 +37,49 @@ gsap.to(".hero svg", {
 	);
 });
 
-gsap.fromTo(
-	".levelup > h2",
-	{
-		fontSize: "7rem",
-	},
-	{
-		fontSize: "3rem",
-		duration: 4,
-		ease: CustomEase.create(
-			"custom",
-			"M0,0 C0,0 0.012,-0.018 0.365,0 0.505,0.007 0.546,0.433 0.713,0.685 0.843,0.883 1,1 1,1 "
-		),
-		scrollTrigger: {
-			trigger: ".levelup > h2",
-			start: "top 100%",
-			toggleActions: "play none none reverse",
+if (screen.width > 450) {
+	gsap.fromTo(
+		".levelup > h2",
+		{
+			fontSize: "7rem",
 		},
-	}
-);
+		{
+			fontSize: "3rem",
+			duration: 4,
+			ease: CustomEase.create(
+				"custom",
+				"M0,0 C0,0 0.012,-0.018 0.365,0 0.505,0.007 0.546,0.433 0.713,0.685 0.843,0.883 1,1 1,1 "
+			),
+			scrollTrigger: {
+				trigger: ".levelup > h2",
+				start: "top 100%",
+				toggleActions: "play none none reverse",
+			},
+		}
+	);
 
-gsap.fromTo(
-	".levelup button",
-	{
-		opacity: 0,
-		y: -50,
-	},
-	{
-		y: 30,
-		opacity: 1,
-		duration: 6,
-		ease: CustomEase.create(
-			"custom",
-			"M0,0 C0,0 0.012,-0.018 0.365,0 0.505,0.007 0.546,0.433 0.713,0.685 0.843,0.883 1,1 1,1 "
-		),
-		scrollTrigger: {
-			trigger: ".levelup > h2",
-			start: "top 100%",
-			toggleActions: "play none none reverse",
+	gsap.fromTo(
+		".levelup a",
+		{
+			opacity: 0,
+			y: -50,
 		},
-	}
-);
+		{
+			y: 30,
+			opacity: 1,
+			duration: 6,
+			ease: CustomEase.create(
+				"custom",
+				"M0,0 C0,0 0.012,-0.018 0.365,0 0.505,0.007 0.546,0.433 0.713,0.685 0.843,0.883 1,1 1,1 "
+			),
+			scrollTrigger: {
+				trigger: ".levelup > h2",
+				start: "top 100%",
+				toggleActions: "play none none reverse",
+			},
+		}
+	);
+}
 
 //arrow fade
 arrowdownhero.style.opacity = 1;
